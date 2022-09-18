2daca1c7634094846d073238f2675bd8381f0b05
Masanari Miura <m0s0n0r0h01h01@icloud.com>
2022-09-18 22:58:28 +0900

    修正

--- .history/resources/views/components/auth-validation-errors.blade_20220918225809.php
+++ .history/resources/views/components/auth-validation-errors.blade_20220918225809.php
@@ -3,8 +3,8 @@
 @if ($errors->any())
     <div {{ $attributes }}>
         <div class="font-medium text-red-600">
-            {{ __('Whoops! Something went wrong.') }}
-        </div>
+        {{ __('エラーの内容を確認してください。') }}
+    </div>
 
         <ul class="mt-3 list-disc list-inside text-sm text-red-600">
             @foreach ($errors->all() as $error)
